3e4e4d9f68651c7ba181e2cbc3050c1d48fc710f
chloehd <chloehipeau@gmail.com>
2020-10-18 22:53:09 +0200

    modify routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PokemonsComponent } from "../app/pokemons/pokemons.component";
 import { PokemonDetailComponent } from "../app/pokemon-detail/pokemon-detail.component";
 
 
 const routes: Routes = [
-  { path: '', component: PokemonsComponent, pathMatch: 'full' },
-  { path: 'pokemon', component: PokemonsComponent },
   { path: 'pokemon/:id', component: PokemonDetailComponent },
   { path: '**', redirectTo: '' }
 ];
