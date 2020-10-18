11bd8b8130c26fe2182c0e5454d731da62f25388
chloehd <chloehipeau@gmail.com>
2020-10-18 16:06:43 +0200

    create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PokemonsComponent } from "../app/pokemons/pokemons.component";
+import { PokemonDetailComponent } from "../app/pokemon-detail/pokemon-detail.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: PokemonsComponent, pathMatch: 'full' },
+  { path: 'pokemon', component: PokemonsComponent },
+  { path: 'pokemon/:id', component: PokemonDetailComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
